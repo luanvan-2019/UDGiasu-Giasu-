b1af4e8c86bf470de5b99cfe904311784930f558
luanvan-2019 <motivation2019@gmail.com>
2019-09-11 02:22:21 -0700

    update lan 1 udgs

---
+++ app/src/main/java/com/udgs12/udgiasu/view/dangky_giasu.java
@@ -0,0 +1,16 @@
+package com.udgs12.udgiasu.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.udgs12.udgiasu.R;
+
+public class dangky_giasu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dangky_giasu);
+    }
+}
